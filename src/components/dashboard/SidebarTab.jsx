c3441a96f076fc0fc9268b226c44ee6da100aeab
bugstile <bugstile@gmail.com>
2025-03-28 06:54:12 +0100

    Small fix to sidebar not exapnding fully to bottom

--- src/components/dashboard/SidebarTab.jsx
+++ src/components/dashboard/SidebarTab.jsx
@@ -4,7 +4,7 @@ export default function SidebarTab({ link, text, icon }) {
   return (
     <NavLink
       to={link}
-      className="flex items-center gap-x-2 transition-all duration-300 ease hover:text-primary m-1 focus:outline-none focus:ring-0 focus:text-primary"
+      className="flex items-center gap-x-2 focus:translate-y-[-1px] hover:translate-y-[-1px] transition-transform transform hover:text-primary m-1 focus:outline-none focus:ring-0 focus:text-primary"
     >
       {icon}
       <p className="text-xl font-semibold">{text}</p>
